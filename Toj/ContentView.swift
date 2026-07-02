15526948206704ae01c9761c497841b5e94f7c48
Muhammadjon Marufov <marufov254@gmail.com>
2026-07-02 17:11:52 +0500

    Initial Commit

---
+++ Toj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Toj
+//
+//  Created by Muhammad Marufov on 7/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Toj/TojApp.swift
@@ -0,0 +1,17 @@
+//
+//  TojApp.swift
+//  Toj
+//
+//  Created by Muhammad Marufov on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
